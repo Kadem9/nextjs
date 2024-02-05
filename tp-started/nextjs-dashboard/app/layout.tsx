a35df3085ec3898e59ca25e8dd2a689e455cc456
Kadem Garnier <kadem.garnier@gmail.com>
2024-02-05 09:20:39 +0100

    first commit

---
+++ tp-started/nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,13 @@
+import '@/app/ui/global.css'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
