d0a44720295e8f70820419daf4a15ea8636cb135
Kadem Garnier <kadem.garnier@gmail.com>
2024-02-06 14:47:39 +0100

    commencement tp final

---
+++ tp-final/tpfinal/src/app/accueil/page.tsx
@@ -0,0 +1,9 @@
+function Page() {
+    return (
+        <div>
+            <h1>Accueil</h1>
+        </div>
+    );
+}
+
+export default Page;
---
+++ tp-final/tpfinal/src/app/ui/dashboard/navbar.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faHospitalUser, faClock } from '@fortawesome/free-solid-svg-icons';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+function NavBar() {
+    const pathname = usePathname();
+
+    return (
+        <nav className="bg-emerald-950 p-4">
+            <div className="flex justify-between items-center">
+                <div className="logo">
+                    <Link href="/" >
+                        <Image
+                            src={"/medical-team.png"}
+                            width={40}
+                            height={40}
+                            alt="Logo de l'application"
+                        />
+                    </Link>
+                </div>
+
+                <div className="navigation flex">
+                    <Link href="/accueil" className={clsx('mr-2 p-2', { 'bg-red-600 text-red-300': pathname === "/rendez-vous" })} >
+                        <span className='flex items-center'>
+                            <FontAwesomeIcon width={20} icon={faHouse} className='mr-1' />
+                            Accueil
+                        </span>
+                    </Link>
+                    <Link href="/patient" className={clsx('mr-2 p-2', { 'bg-sky-100 text-red-300': pathname === "/rendez-vous" })} >
+                        <span className='flex items-center'>
+                            <FontAwesomeIcon width={20} icon={faHospitalUser} className='mr-1' />
+                            Patients
+                        </span>
+                    </Link>
+                    <Link href="/rendez-vous" className={clsx('mr-2 p-2', { 'bg-sky-100 text-red-300': pathname === "/rendez-vous" })} >
+                        <span className='flex items-center'>
+                            <FontAwesomeIcon width={20} icon={faClock} className='mr-1' />
+                            Rendez-vous
+                        </span>
+                    </Link>
+                </div>
+
+                <div className="doctor-login">
+
+                </div>
+
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
