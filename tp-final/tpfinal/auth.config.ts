47c5a22e01cfff7ba5b77326aa306d01f33516c1
Kadem Garnier <kadem.garnier@gmail.com>
2024-02-08 17:24:50 +0100

    fin de journée

--- tp-final/tpfinal/auth.config.ts
+++ tp-final/tpfinal/auth.config.ts
@@ -8,14 +8,15 @@ export const authConfig = {
         authorized({ auth, request: { nextUrl } }) {
             const isLoggedIn = !!auth?.user;
             const isOnHome = nextUrl.pathname.startsWith('/accueil');
+
             if (isOnHome) {
                 if (isLoggedIn) return true;
-                return false; // Redirect unauthenticated users to login page
+                return false;
             } else if (isLoggedIn) {
                 return Response.redirect(new URL('/accueil', nextUrl));
             }
             return true;
         },
     },
-    providers: [], // Add providers with an empty array for now
+    providers: [],
 } satisfies NextAuthConfig;
